2cedc70fb0048ec3d07a2efd9b67be3910e7cc2a
Denzel97 <denzelwash97@gmail.com>
2019-03-05 09:53:40 +0300

    changed the urls

--- galla/urls.py
+++ galla/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns=[
-    url('^$',views.welcome,name='welcome'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_galla,name = 'pastGalla'),
-    url(r'^search/', views.search_results, name='search_results'),
+    url('^$',views.welcome,name='galla'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
